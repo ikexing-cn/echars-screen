bc50d0927dff20d9db169f97860f1f070270ccbb
ikexing <ikexing@qq.com>
2022-09-15 13:26:43 +0800

    feat: init

---
+++ src/layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div flexc flex-col text-gray-500>
+    <RouterView />
+    <Footer />
+  </div>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const { increment, decrement, count } = $(useCount())
+</script>
+
+<template>
+  <div>hello world</div>
+  <div flexc children-ml-2 mt-2>
+    <button btn @click="increment">
+      increment
+    </button>
+
+    <span>{{ count }}</span>
+
+    <button btn @click="decrement">
+      decrement
+    </button>
+  </div>
+</template>
